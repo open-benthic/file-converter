b29a37177ed8da0815b8e16d0cc21e1525aa415d
Majid <majid@Majids-iMac.local>
2020-03-23 18:17:32 +0430

    Add some changes to package.json

--- dist/FileConverter.js
+++ dist/FileConverter.js
@@ -23,34 +23,52 @@ class HomePage extends PureComponent {
 
   convertFilesToBase64(files) {
     let ef = files;
+
     if (this.state.multipleFiles) {
-      let files = { base64: [], fileList: ef };
+      let files = {
+        base64: [],
+        fileList: ef
+      };
+
       for (var i = 0, len = ef.length; i < len; i++) {
         let reader = new FileReader();
         let f = ef[i];
+
         reader.onloadend = e => {
           files.base64.push(reader.result);
+
           if (files.base64.length === ef.length) {
             this._logger(files);
           }
         };
+
         reader.readAsDataURL(f);
       }
     } else {
-      let files = { base64: "", fileList: ef };
+      let files = {
+        base64: "",
+        fileList: ef
+      };
       let f = ef[0];
       let reader = new FileReader();
+
       reader.onloadend = e => {
         files.base64 = reader.result;
+
         this._logger(files);
       };
+
       reader.readAsDataURL(f);
     }
   }
 
   render() {
-    return <input type="file" onChange={this.handleFiles} />;
+    return /*#__PURE__*/React.createElement("input", {
+      type: "file",
+      onChange: this.handleFiles
+    });
   }
+
 }
 
 export default HomePage;
